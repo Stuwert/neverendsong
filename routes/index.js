39e222ae4693f39a63b7fb26c91e162b09220c31
Stuwert <stuart@urback.net>
2015-12-23 20:06:51 -0800

    Complete website

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  musicObj = new musicObjMaker(1, "It's the song that never ends");
+  res.render('index', musicObj)
 });
 
-router.get('/verse-2', function(req, res, next){
-  res.render('index', { title: 'Verse 2' })
+router.get('/verse-2', function(req, res){
+  musicObj = new musicObjMaker(2, "It goes on and on my friends");
+  res.render('index', musicObj)
 })
+
+router.get('/verse-3', function(req, res) {
+  musicObj = new musicObjMaker(3, "People started singing it not knowing what it was");
+  res.render('index', musicObj)
+});
+
+router.get('/verse-4', function(req, res){
+  musicObj = new musicObjMaker(4, "And they'll continue singing it forever just because...");
+  res.render('index', musicObj)
+})
+
+function musicObjMaker(currentnumber, verse){
+  this.title = verse;
+  this.image = currentnumber === 1 ? "images/song-never-ends-" + currentnumber + ".jpg" : "../images/song-never-ends-" + currentnumber + ".jpg";
+  var nextnumber = currentnumber + 1;
+  this.next = nextnumber > 4 ? "/" : "/verse-" + nextnumber + "/";
+}
 
 module.exports = router;
